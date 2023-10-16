3eaabe4436065911607c72ad4ce3620383b02ef2
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2023-10-16 03:33:08 -0400

    mend

--- src/inputs/RadioInput.jsx
+++ src/inputs/RadioInput.jsx
@@ -20,7 +20,7 @@ function RadioField({ objectKey, value, name, checked }) {
       <input
         type="radio"
         onChange={handleInputChange}
-        id={`${value}-font`}
+        id={value}
         name={name}
         value={value}
         defaultChecked={checked}
